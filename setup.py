d6ef35c01555c66c7327e16c4be699c652d12018
Tal Wrii <talwrii@gmail.com>
2025-06-03 14:41:30 +0200

    new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import distutils.core
 
 setuptools.setup(
     name='clipboard-many-files',
-    version="1.0.1",
+    version="1.0.2",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
